8e512e683c7bf0fd09a034f83b722c558d9b4d5f
alenaver16 <30392723+alenaver16@users.noreply.github.com>
2017-08-15 21:34:35 +0300

    UnityProject
    UnityProject

---
+++ Assets/Scripts/ArmRaise.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmRaise : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject Hand;
+    private int count = 0;
+    private float _clickDelay = 0.9f;
+    private float _clickTime;
+
+    private void Start()
+    {
+        anim = Hand.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        CheckClicks();
+    }
+    private void CheckClicks()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            count++;
+            _clickTime = 0;
+        }
+
+        if (count == 0) return;
+
+        if (_clickTime < _clickDelay)
+        {
+            _clickTime += Time.deltaTime;
+            return;
+        }
+
+        HandleClicks(count);
+        count = 0;
+        _clickTime = 0;
+
+    }
+    private void HandleClicks(int count)
+    {
+        if (count == 5)
+        {
+            anim.SetTrigger("ArmRaise");
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/UImanager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UImanager : MonoBehaviour {
+    public void OpenBlacksmith() { SceneManager.LoadScene("Blacksmish"); }
+    public void OpenDeepspace() { SceneManager.LoadScene("Deepspace"); }
+    public void OpenMenu() { SceneManager.LoadScene("Menu"); }
+    public void Close()
+    {
+        //GUI.Label(new Rect(50, 10, 180, 30), "Вы уже выходите?");
+        //    if (GUI.Button(new Rect(10, 40, 180, 30), "Да"))
+        //    {
+                Application.Quit();
+            //}
+            //if (GUI.Button(new Rect(10, 80, 180, 30), "Нет"))
+            //{
+            //    SceneManager.LoadScene("Menu");
+            //}
+    }
+}
