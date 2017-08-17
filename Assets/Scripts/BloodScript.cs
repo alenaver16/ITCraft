ff0bb586e61444db772e94ec64786fdd28840759
VladSerhiienko24 <holms9853@gmail.com>
2017-08-18 00:09:21 +0300

    unity project
    unity project

---
+++ Assets/Scripts/BloodScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodScript : MonoBehaviour
+{
+    public ParticleSystem particle = null;
+    
+    void Update()
+    {
+        if (Input.GetMouseButton(0) || Input.touchCount == 1) 
+        {
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+            RaycastHit hit;
+            if ((Physics.Raycast(ray, out hit, Mathf.Infinity)) && (hit.collider.name == "HeadSphere"))
+            {
+             ParticleSystem newParticle = Instantiate(particle, hit.point, transform.rotation, hit.transform);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    private Animator anim;
+    public GameObject character;
+
+    public float maxTime;
+    public float minDist;
+
+    float startTime;
+    float finishTime;
+
+    Vector3 startPos;
+    Vector3 finishPos;
+
+    Vector2 dist;
+
+    float swipeDist;
+    float swipeTime;
+
+    float passedTime;
+
+private void Start()
+{
+    anim = character.GetComponent<Animator>();
+
+}
+
+private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetTrigger("run"); passedTime = 0; return;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            anim.SetTrigger("stop"); passedTime = 0; return;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetTrigger("runleft"); passedTime = 0; return;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetTrigger("runright"); passedTime = 0; return;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("jump"); passedTime = 0; return;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            anim.SetTrigger("crouch"); passedTime = 0; return;
+        }
+
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                startTime = Time.time;
+                startPos = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                finishTime = Time.time;
+                finishPos = touch.position;
+
+                swipeDist = (startPos - finishPos).magnitude;
+                swipeTime = startTime - finishTime;
+
+                if( swipeTime < maxTime && swipeDist > minDist )
+                {
+                    swipe();
+                }
+            }
+            passedTime = 0; return;
+        }
+
+        if (Input.touchCount == 2)
+        {
+            Touch touch_1 = Input.GetTouch(0);
+            Touch touch_2 = Input.GetTouch(1);
+
+            if (touch_1.phase == TouchPhase.Began && touch_2.phase == TouchPhase.Began)
+            {
+                anim.SetTrigger("jump");
+                passedTime = 0; return;
+            }
+            
+        }
+
+        if(passedTime < maxTime)
+        {
+            passedTime += Time.deltaTime; return;
+        }
+        else if(passedTime > maxTime)
+        {
+            anim.SetTrigger("crouch");
+            passedTime = 0; return;
+        }
+}
+    public void swipe()
+    {
+        dist = finishPos - startPos;
+        if(Mathf.Abs(dist.x) > Mathf.Abs(dist.y))
+        {
+            if (dist.x > 0)
+            {
+                anim.SetTrigger("runright"); 
+
+            }
+            if (dist.x < 0)
+            {
+                anim.SetTrigger("runleft"); 
+            }
+        }
+        else if (Mathf.Abs(dist.x) < Mathf.Abs(dist.y))
+        {
+            
+            if (dist.y > 0)
+            {
+                anim.SetTrigger("run");
+            }
+            if (dist.y < 0)
+            {
+                anim.SetTrigger("stop");
+            }
+        }
+    }
+}
